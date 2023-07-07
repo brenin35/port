01555bd925cd5f508d9e65820fed9422945d24ae
brenin35 <brenoca35@gmail.com>
2023-07-07 14:46:31 -0300

    About us, section ou solutions

--- codigo/js/btnToggle.js
+++ codigo/js/btnToggle.js
@@ -20,3 +20,18 @@ document.addEventListener("DOMContentLoaded",function(){
         logoImage.style.filter = "invert(1)";
     }
 });
+
+let checkbox = document.querySelector("input[name=theme_switch]");
+let imgMain = document.querySelector(".img-main");
+let background = document.querySelector("#background");
+
+checkbox.addEventListener("change", function() {
+    if (checkbox.checked) {
+        imgMain.src = "img/Construction-inverted.png";
+        background.style.backgroundImage = "url(/img/DJI_0030.png)";
+    } else {
+        imgMain.src = "img/Construction-not-inverted.png";
+        background.style.backgroundImage = "url(/img/DJI_0030_branco.png)";
+    }
+});
+
